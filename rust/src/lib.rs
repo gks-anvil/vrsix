5ef75fcc776b7a2776f4f6f3c5c97a7bed4a5300
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-01-27 10:13:36 -0500

    cicd: fix release workflow (#26)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -24,10 +24,10 @@ create_exception!(loading_module, FiletypeError, VrsixError);
 #[pyo3(name = "_core")]
 fn loading_module(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     let _ = m.add_function(wrap_pyfunction!(vcf_to_sqlite, m)?);
-    m.add("VrsixError", py.get_type_bound::<VrsixError>())?;
-    m.add("SqliteFileError", py.get_type_bound::<SqliteFileError>())?;
-    m.add("VcfError", py.get_type_bound::<VcfError>())?;
-    m.add("VrsixDbError", py.get_type_bound::<VrsixDbError>())?;
-    m.add("FiletypeError", py.get_type_bound::<FiletypeError>())?;
+    m.add("VrsixError", py.get_type::<VrsixError>())?;
+    m.add("SqliteFileError", py.get_type::<SqliteFileError>())?;
+    m.add("VcfError", py.get_type::<VcfError>())?;
+    m.add("VrsixDbError", py.get_type::<VrsixDbError>())?;
+    m.add("FiletypeError", py.get_type::<FiletypeError>())?;
     Ok(())
 }
