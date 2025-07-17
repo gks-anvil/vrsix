3dc6df5792754cff0cdd63a3214b5a57a7305e31
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-07-17 07:22:55 -0400

    feat: declare version + provide in CLI

--- src/vrsix/__init__.py
+++ src/vrsix/__init__.py
@@ -1,1 +1,10 @@
 """Tools for constructing and interacting with sqlite-based indexing of VRS-annotated VCFs."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("thera-py")
+except PackageNotFoundError:
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
